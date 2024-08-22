b992cb54c7fb236baf9f26736391401d9d81530c
Rostyslav <141353047+savage0003@users.noreply.github.com>
2024-08-22 11:25:29 +0100

    third commit

--- carts/views.py
+++ carts/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product
 from .models import Cart, CartItem
 
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 def _cart_id(request):
     cart = request.session.session_key
@@ -33,7 +33,23 @@ def add_cart(request, product_id):
         cart_item.save()
     return redirect('cart')
 
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
 
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('cart')
 
 def cart(request, total=0, quantity=0, tax=0, grand_total=0, cart_items=None):
     try:
@@ -45,13 +61,13 @@ def cart(request, total=0, quantity=0, tax=0, grand_total=0, cart_items=None):
         tax = (2 * total)/100
         grand_total = total + tax
     except Cart.DoesNotExist:
-        cart_items =[]
+        cart_items = []
 
     context = {
         'total': total,
         'quantity': quantity,
         'cart_items': cart_items,
-        'tax'       : tax,
+        'tax': tax,
         'grand_total': grand_total,
     }
     return render(request, 'store/cart.html', context)
