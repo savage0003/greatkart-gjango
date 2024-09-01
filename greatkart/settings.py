f9cf594c7ca027b75e3994c77766d9f2a2010b3c
Rostyslav <141353047+savage0003@users.noreply.github.com>
2024-09-01 10:45:32 +0100

    finished messages after registration

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+from django.contrib.messages import constants as messages
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -139,4 +141,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_DIR = BASE_DIR /'media'
 
-
+MESSAGE_TAGS ={
+    messages.ERROR: 'danger',
+}
